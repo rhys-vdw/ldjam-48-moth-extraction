dd0cd533bd1a6d9202acaf4057b833f8a7c424c4
Rhys van der Waerden <rhys.vdw@gmail.com>
2021-04-25 23:04:40 +1000

    Add handle

--- Assets/_Game/Scripts/Draggable.cs
+++ Assets/_Game/Scripts/Draggable.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 
 namespace Moth {
   public class Draggable : MonoBehaviour {
-    [SerializeField] SpriteRenderer _spriteRenderer = null;
+    [SerializeField] SpriteRenderer[] _spriteRenderers = null;
     Rigidbody2D _rigidbody2D = null;
     Camera _camera;
     Vector2 _prevPosition;
     bool _isDragging = false;
     static readonly Plane _plane = new Plane(Vector3.back, Vector3.zero);
+    Color _defaultColor;
+    [SerializeField] Color _dragColor = Color.white;
 
     public static bool IsAnyDragging { get; private set; }
 
     void Awake() {
       _camera = Camera.main;
       _rigidbody2D = GetComponent<Rigidbody2D>();
+      _defaultColor = _spriteRenderers[0].color;
     }
 
     Vector2 GetPosition() {
@@ -32,15 +35,19 @@ namespace Moth {
       _isDragging = true;
       IsAnyDragging = true;
       // _rigidbody2D.isKinematic = true;
-      _spriteRenderer.color = Color.red;
+      SetColor(_dragColor);
+    }
+
+    void SetColor(Color color) {
+      foreach (var r in _spriteRenderers) r.color = color;
     }
 
     void Update() {
       if (_isDragging && Input.GetMouseButtonUp(0)) {
         _isDragging = false;
         IsAnyDragging = false;
-        _rigidbody2D.isKinematic = false;
-        _spriteRenderer.color = Color.white;
+        // _rigidbody2D.isKinematic = false;
+        SetColor(_defaultColor);
       }
     }
 
