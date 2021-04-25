b897fdbaae8bf11001c072df349a95fe06f8a35d
Rhys van der Waerden <rhys.vdw@gmail.com>
2021-04-26 01:03:51 +1000

    Game over

---
+++ Assets/_Game/Scripts/Brain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Moth {
+  public class Brain : MonoBehaviour
+  {
+    [SerializeField] GameObject _splat = null;
+    public bool IsDead { get; private set; } = false;
+
+    void Awake() {
+      _splat.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+      if (IsDead) return;
+
+      if (other.TryGetComponent<Moth>(out var moth)) {
+        moth.IsFlapping = false;
+        moth.IsWiggling = false;
+        foreach (var r in moth.GetComponentsInChildren<Rigidbody2D>()) {
+          r.simulated = false;
+        }
+      }
+      _splat.SetActive(true);
+      IsDead = true;
+      StartCoroutine(RestartCoroutine());
+    }
+
+    IEnumerator RestartCoroutine() {
+      yield return new WaitForSeconds(5f);
+      SceneManager.LoadScene(0);
+    }
+  }
+}
--- Assets/_Game/Scripts/MusicController.cs
+++ Assets/_Game/Scripts/MusicController.cs
@@ -3,8 +3,16 @@
 namespace Moth {
   public class MusicController : MonoBehaviour {
     [SerializeField] AudioSource _source;
+    Brain _brain;
+
+    void Awake() {
+      _brain = FindObjectOfType<Brain>();
+    }
 
     void Update() {
+      if (_brain.IsDead) {
+        _source.Stop();
+      }
       if (Draggable.IsAnyDragging) {
         if (!_source.isPlaying) {
           _source.Play();
